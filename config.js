d2ac6b6fb5a5a38d175ef39fc1feb83480ad47b5
Botjz1 <118467025+Botjz1@users.noreply.github.com>
2023-05-13 20:57:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['918590218598', 'HANAN', true],
+  ['918598218598', 'Hanuman', true], 
   [''] 
 ] //Number of owners
 
